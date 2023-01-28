613f2ef243a950063efe56087f43c2b496dbd990
Mahmud Ridwan <m@hjr265.me>
2023-01-28 23:00:00 +0600

    Add configuration defaults

--- config.go
+++ config.go
@@ -8,29 +8,63 @@ import (
 )
 
 type Config struct {
-	Printd struct {
-		FontSize     int
-		LineHeight   float64
-		MarginTop    float64
-		MarginRight  float64
-		MarginBottom float64
-		MarginLeft   float64
-		TabSize      int
-		KeepPDF      bool
-		DelayAfter   time.Duration
-	}
-	Printer struct {
-		Name     string
-		PageSize PageSize
-	}
-	Toph struct {
-		BaseURL   string
-		Token     string
-		ContestID string
-	}
+	Printd  ConfigPrintd
+	Printer ConfigPrinter
+	Toph    ConfigToph
+}
+
+func (c *Config) initDefaults() {
+	c.Printd.initDefaults()
+	c.Printer.initDefaults()
+	c.Toph.initDefaults()
+}
+
+type ConfigPrintd struct {
+	FontSize     int
+	LineHeight   float64
+	MarginTop    float64
+	MarginRight  float64
+	MarginBottom float64
+	MarginLeft   float64
+	TabSize      int
+	KeepPDF      bool
+	DelayAfter   time.Duration
+}
+
+func (c *ConfigPrintd) initDefaults() {
+	c.FontSize = 13
+	c.LineHeight = 20
+	c.MarginTop = 50
+	c.MarginRight = 25
+	c.MarginBottom = 50
+	c.MarginLeft = 25
+	c.TabSize = 4
+	c.KeepPDF = true
+	c.DelayAfter = 500 * time.Millisecond
+}
+
+type ConfigPrinter struct {
+	Name     string
+	PageSize PageSize
+}
+
+func (c *ConfigPrinter) initDefaults() {
+	c.PageSize = "A4"
+}
+
+type ConfigToph struct {
+	BaseURL   string
+	Token     string
+	ContestID string
+}
+
+func (c *ConfigToph) initDefaults() {
+	c.BaseURL = "https://toph.co"
 }
 
 func parseConfig() (cfg Config, err error) {
+	cfg.initDefaults()
+
 	b, err := os.ReadFile(flagConfig)
 	if err != nil {
 		return
